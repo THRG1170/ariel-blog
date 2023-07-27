21ac19cf7134c90d5005bab2bf43495f4956b725
THRG1170 <trahmadisyawal@gmail.com>
2023-07-27 18:57:14 +0800

    add an landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+
 import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 import "./App.css";
@@ -19,7 +20,22 @@ function App() {
     <>
       <Navbar></Navbar>
       <main>
-        <section id="perkenalan">perkenalan</section>
+        <section id="perkenalan">
+          <div className="perkenalan">
+            <h1>Hi saya Ariel Hutomo</h1>
+            <small>Murid SMA NEGERI 1 PALU</small>
+            <p>
+              Nama panjang saya Ariel Hutomo Daeng Malongi. Saya murid SMA
+              NEGERI 1 PALU.Saya memiliki minat dalam seni menggambar dan
+              keterampilan berbicara di depan umum yang baik (public speaking).
+              Selain itu, saya juga memiliki kemampuan dalam hal surat menyurat.
+              Saya sangat menikmati proses kreatif dalam menggambar dan merasa
+              percaya diri saat berbicara di hadapan audiens. Kemampuan menyusun
+              komunikasi tertulis juga menjadi nilai tambah dalam berkomunikasi.
+            </p>
+          </div>
+          <img src="./arielhutomo.png" alt="foto ariel" className="fotoaril" />
+        </section>
         <section id="skill">skill</section>
       </main>
       <footer id="contact">ini contact</footer>
