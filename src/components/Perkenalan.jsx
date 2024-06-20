f283bf50379765fd4ebb67d5e0bd494d287a0016
THRG1170 <trahmadisyawal@gmail.com>
2024-06-20 22:46:09 +0800

    haha

--- src/components/Perkenalan.jsx
+++ src/components/Perkenalan.jsx
@@ -11,15 +11,15 @@ function Pekernalan() {
             Nama panjang saya <span>Ariel Hutomo Daeng Malongi</span>. Saya
             murid <span>SMA NEGERI 1 PALU</span>. Saya tinggal di Sulawesi
             Tengah, Kota Palu, Jln. Dr. Wahidin. <br />
-            Ayo liat sekitar dan mengetahui siapa saya!
+            Ayo liat sekitar dan mengetahui siapa gweh!
           </p>
           <a href="#skill" className="cta">
             Get started
           </a>
         </div>
         <img src="./arielhutomo.png" alt="foto ariel" className="fotoaril" />
-        <div class="animateme">
-          <ul class="bg-bubbles">
+        <div className="animateme">
+          <ul className="bg-bubbles">
             <li></li>
             <li></li>
             <li></li>
